7cae26fb5ad2ac505df539ebd2329e3d91c698e8
neogulcity <neogulcity@gmail.com>
2022-08-04 19:40:36 +0900

    열거형 클래스 eDataType 추가

--- src/Json_Write.cpp
+++ src/Json_Write.cpp
@@ -4,9 +4,26 @@
 #include "Json.h"
 #include "Log.h"
 
+#include "DataType.h"
+
 using namespace std;
 
 namespace jsonparser {
+    // 열거형 클래스 eDatatype 데이터 타입을 구분할 때 사용.
+    enum class eDataType {
+        ePointer,
+        eString,
+        eElse
+    };
+
+    template <typename T>
+    eDataType GetDataType(T _param)
+    {
+        eDataType result;
+
+        return result;
+    }
+
     void json::AddText(const string& _text)
     {
         mstrText = mstrText + _text;
@@ -33,21 +50,28 @@ namespace jsonparser {
         SetText(strText);
     }
 
+    void json::Write()
+    {
+        ofstream out("result.json");
+        out << GetText();
+        out.close();
+    }
+
     void main_write()
     {
         // log_debug("Write의 기능 테스트를 위한 main 함수입니다.");
 
-        int myInt = 10; 
-        size_t mySize = 1;
-        double myDouble = 3.3;
+        // int myInt = 10; 
+        // size_t mySize = 1;
+        // double myDouble = 3.3;
 
-        json file;
-        file.Serialize(myInt, "myInt");
-        file.Serialize(mySize, "mySize");
-        file.Serialize(myDouble, "myDouble");
+        // json file;
+        // file.Serialize(myInt, "myInt");
+        // file.Serialize(mySize, "mySize");
+        // file.Serialize(myDouble, "myDouble");
 
-        ofstream out("result.json");
-        out << file.GetText();
-        out.close();
+        // GetDataType(myInt);
+
+        // file.Write();
     }
 }
