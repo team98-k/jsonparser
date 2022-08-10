ec206afae3413741c364279321b58f9c957fb035
neogulcity <neogulcity@gmail.com>
2022-08-10 14:13:04 +0900

    Update DataType.h

--- src/DataType.h
+++ src/DataType.h
@@ -5,4 +5,10 @@
 template <class T>
 std::string GetTypeName();
 
+template <class T, class S>
+struct is_string;
+
+template <class T, class Traits, class Alloc>
+struct is_string<T, std::basic_string<T, Traits, Alloc>>;
+
 #include "DataType.hpp"
